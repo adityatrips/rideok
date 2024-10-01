6028fc12415b5883ef143d92682ca699c008ff36
Aditya <adityatripathi.at04@gmail.com>
2024-10-02 01:58:59 +0530

    Refactor metadata in RootLayout component

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,6 +6,22 @@ export const metadata = {
 	title: "RideOk - A ride sharing app",
 	description:
 		"RideOk is a ride sharing app that connects drivers and passengers in a close community such as your society or workplace.",
+	keywords: [
+		"ride sharing",
+		"carpooling",
+		"ride sharing app",
+		"carpooling app",
+	],
+	author: [
+		{
+			name: "Aditya Tripahti",
+			url: "https://adityatripathi.vercel.app",
+		},
+		{
+			name: "Ayush Verma",
+			url: "https://ayush-verma-1708.netlify.app",
+		},
+	],
 };
 
 export default function RootLayout({ children }) {

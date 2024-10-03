45f6b11990d6550b2d650429a4365f0fed2968a1
Aditya <adityatripathi.at04@gmail.com>
2024-10-03 19:49:14 +0530

    Refactor .env file and add ACCESS_KEY

--- app/api/feedback/route.js
+++ app/api/feedback/route.js
@@ -23,3 +23,47 @@ export const POST = async (req) => {
 		);
 	}
 };
+
+export const GET = async (req) => {
+	connectToDb();
+
+	const accessId = req.nextUrl.searchParams.get("accessId");
+
+	console.log(`Access ID: ${accessId}`);
+	console.log(`Actual ID: ${process.env.ACCESS_KEY}`);
+
+	if (accessId !== process.env.ACCESS_KEY) {
+		return Response.json({ message: "Unauthorized access." }, { status: 401 });
+	}
+
+	try {
+		const feedbacks = await Feedback.find();
+
+		return Response.json(feedbacks, { status: 200 });
+	} catch (error) {
+		return Response.json(
+			{ message: error ?? "Error fetching feedbacks." },
+			{ status: 500 }
+		);
+	}
+};
+
+export const DELETE = async (req) => {
+	connectToDb();
+
+	const id = req.nextUrl.searchParams.get("id");
+
+	try {
+		await Feedback.findByIdAndDelete(id);
+
+		return Response.json(
+			{ message: "Feedback deleted successfully!" },
+			{ status: 200 }
+		);
+	} catch (error) {
+		return Response.json(
+			{ message: error ?? "Error deleting feedback." },
+			{ status: 500 }
+		);
+	}
+};
